04939f0ea0efbd907461e3a7a93b4e7e26e662a0
Spectrasonic <spectrasonic117@gmail.com>
2025-03-26 12:47:54 -0600

    feat(ImpostorTrait): add sound effect and adjust title timings
    Add sound effect when a player right-clicks an impostor NPC using SoundUtils.
    Adjust title display timings for better user experience.

--- src/main/java/com/spectrasonic/AmongUs/Traits/ImpostorTrait.java
+++ src/main/java/com/spectrasonic/AmongUs/Traits/ImpostorTrait.java
@@ -5,6 +5,8 @@ import net.citizensnpcs.api.trait.Trait;
 import net.citizensnpcs.api.trait.TraitName;
 import org.bukkit.event.EventHandler;
 import com.spectrasonic.AmongUs.Utils.MessageUtils;
+import com.spectrasonic.AmongUs.Utils.SoundUtils;
+import org.bukkit.Sound;
 
 @TraitName("impostor")
 public class ImpostorTrait extends Trait {
@@ -15,10 +17,12 @@ public class ImpostorTrait extends Trait {
     @EventHandler
     public void onRightClick(NPCRightClickEvent event) {
         if (event.getNPC() == this.getNPC()) {
+            // Correct player reference from event
+            SoundUtils.playerSound(event.getClicker(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 1.0f, 1.0f);
             MessageUtils.sendTitle(event.getClicker(),
                     "<green>¡Atrapaste al impostor!</green>",
                     "",
-                    5, 20, 5);
+                    1, 2, 1);
         }
     }
 }
